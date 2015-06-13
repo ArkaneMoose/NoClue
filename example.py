7b148e64dea50da4c5c0d20ebbf7c08798fc7077
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2015-06-12 23:14:38 -0400

    Added files based on jedevc's bot library.

---
+++ example.py
@@ -0,0 +1,20 @@
+import euphoria as eu
+
+class HiBot(eu.ping_room.PingRoom, eu.chat_room.ChatRoom):
+    def __init__(self, roomname, password=None):
+        super().__init__(roomname, password)
+    
+        self.nickname = "HiBot"
+
+    def handle_chat(self, message):
+        if message["content"] == "!ping":
+            self.send_chat("Pong!", message["id"])
+        elif "hi" in message["content"].lower():
+            self.send_chat("Hi there!", message["id"])
+
+def main():
+    hi = HiBot("test")
+    eu.executable.start(hi)
+
+if __name__ == "__main__":
+    main()
